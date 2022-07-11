c4c34fee00c0d27f3b04255279b4e367c6edbf7f
Tateishi Tadatoshi <ishio39@gmail.com>
2022-07-12 01:12:27 +0900

    tabulate

--- src/csv_report/cli.py
+++ src/csv_report/cli.py
@@ -1,9 +1,9 @@
 import subprocess
 from pathlib import Path
 
-from fire import Fire
-
 import pandas as pd
+from fire import Fire
+from tabulate import tabulate
 
 
 def exec_main():
@@ -11,21 +11,23 @@ def exec_main():
     cmd = "ls -l | sort"
     with filename.open("w") as fp:
         ret = subprocess.run(cmd, shell=True, stdout=fp)
-#    print(ret)
 
 
 def exec_fire():
     Fire(exec_main)
 
 
-def reg_main(file:str = "~/download/main.csv", account:str = "資産:現金:家計財布") -> None:
+def reg_main(file: str = "~/download/main.csv", account: str = "資産:現金:家計財布") -> None:
     df = pd.read_csv(file)
     df = df[df["account"] == account]
     df["amount"] = df["amount"].astype(int)
     df = df.sort_values("date amount".split(), ascending=[True, False])
     df["sum"] = df["amount"].cumsum()
     df = df["date payee account amount sum".split()]
-    print(df)
+    df = df.loc[::-1]
+
+    print(tabulate(df, df.columns, tablefmt="psql"))
+
 
 def reg_fire():
     Fire(reg_main)
